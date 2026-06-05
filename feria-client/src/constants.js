9d8104f7fc14e087c0dedf005597cd36ba640dc4
Ian-nwb <kennethsianghio756@gmail.com>
2026-06-06 05:20:19 +0800

    changed constants

--- feria-client/src/constants.js
+++ feria-client/src/constants.js
@@ -1,4 +1,8 @@
-const HOST = `${import.meta.env.VITE_API_URL}/api`;
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
+const HOST = apiUrl.endsWith('/api') || apiUrl.endsWith('/api/')
+  ? apiUrl.replace(/\/$/, '')
+  : `${apiUrl.replace(/\/$/, '')}/api`;
 
 export default {
     HOST,
