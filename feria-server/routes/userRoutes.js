0f61841f9547edf4c883d148754e4bd67eefba6f
6kenferia-debug <6ken.feria@gmail.com>
2026-06-05 16:40:48 +0800

    Update userRoutes.js

--- feria-server/routes/userRoutes.js
+++ feria-server/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
-// import functions
-const { getUsers, createUser, updateUser, deleteUser, loginUser, } = require('../controllers/userController');
+
+const { getUsers, createUser, updateUser, deleteUser, loginUser, registerUser } = require('../controllers/userController');
+
 const router = express.Router();
+
 router.route('/').get(getUsers).post(createUser);
+
 router.route('/:id').put(updateUser).delete(deleteUser);
+
 router.post('/login', loginUser);
+
+router.post('/register', registerUser);
+
 module.exports = router;
